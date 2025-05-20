6f4796826067afa5c448ecd536f155a2ce00018a
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-05-20 12:12:47 +0200

    fix: Update CORS origin to allow requests from production URL

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:4200',  
+  origin: 'https://cantina.vercel.app',  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],  
   credentials: true,  
